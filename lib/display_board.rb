6848d5e95b6b12965032dc18d70afca9fd04670b
Karl Benbeneck <kbenbeneck@gmail.com>
2018-09-28 15:11:54 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 def display_board(board)
-  puts " {} | {} | {} "
+  puts "   |   |   "
   puts "-----------"
-  puts " {} | {} | {} "
+  puts "   |   |   "
   puts "-----------"
-  puts " {} | {} | {} "
+  puts "   |   |   "
 end
 
 # out the current state.
